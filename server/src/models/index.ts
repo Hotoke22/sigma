bd662f27e2f8729277f793a273b84427feab9c4e
Jorge Alvarez <jalvarez.y.89@gmail.com>
2022-11-10 04:33:36 -0500

    Se reemplaza carpeta server del proyecto del segundo sprint por la carpeta backend del tercer sprint por motivos de actualización mayor

--- server/src/models/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './usuario.model';
-export * from './mascotas.model';
-export * from './plan.model';
-export * from './sucursal.model';
-export * from './prospecto.model';
-export * from './productos-servicios.model';
--- server/src/repositories/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './mascotas.repository';
-export * from './plan.repository';
-export * from './usuario.repository';
-export * from './productos-servicios.repository';
-export * from './prospecto.repository';
-export * from './sucursal.repository';
